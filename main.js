307caa4f1cf7a156b3aab7746b369e5aec5bf3e9
fibbonnaci <jyothinookula@gmail.com>
2016-08-12 22:42:12 -0700

    added answers

--- main.js
+++ main.js
@@ -13,40 +13,60 @@
 
 $(document).ready(function() {  
     
-    
-//1. 
-   
-   
-//2.  
-    
-    
-//3. 
-   
-	    
+//1.
+  $(".alertme").click(function(){
+    alert("jquery seems easy so far");
+  });
+
+//2.
+$(".clickme").click(function(){
+  $(".clickme").text("I was clicked");
+});
+
+
+//3.
+$(".addStyle").click(function(){
+  $("button").addClass("buttonStyle");
+});
+
+
 //4.
- 
-   
-//5. 
+$(".addDifferentStyle").click(function(){
+  $(this).addClass("addUniqueStyle");
+});
 
-	
+
+//5.
+$(".removeBtn").click(function(){
+  $("button").removeClass("addUniqueStyle");
+  $("button").removeClass("buttonStyle");
+
+});
+
+
 //6.
-	
-	
-//7.
+$(".clickToggle").click(function(){
+  $(this).toggleClass("buttonStyle");
+});
 
-	
-	
+
+//7.
+$(".squareBtn").click(function(){
+  $(".square").hide();
+});
+
+
+
 //8.
+$(".circle").click(function(){
+$(this).hide();
+});
 
 
 
 //9.
+$(".toggleRedSquare").click(function(){
+  $(".redSquare").toggle(2000);
+});
 
-
-
-});  // Close: $(document).ready(function() { 
-	
-	
-
-	
-	
+});  // Close: $(document).ready(function() {
